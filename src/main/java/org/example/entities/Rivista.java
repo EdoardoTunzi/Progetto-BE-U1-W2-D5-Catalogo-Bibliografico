452d948cd557987730224eee7e38c4c04cf99519
Edoardo <edfl.tunzi@gmail.com>
2025-01-17 11:07:24 +0100

    classi create

---
+++ src/main/java/org/example/entities/Rivista.java
@@ -0,0 +1,25 @@
+package org.example.entities;
+
+public class Rivista extends ElementoCatalogo {
+    private Periodicita periodicita;
+
+    public Rivista(String isbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(isbn, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Rivista{" +
+                "periodicita=" + periodicita +
+                '}';
+    }
+}
